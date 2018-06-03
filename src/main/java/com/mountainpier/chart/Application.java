780078e40842bce0725c6778ded61d10f5d7f957
matthewpoletin <contact@matthewpoletin.ru>
2018-06-03 19:43:14 +0300

    Empty spring project

---
+++ src/main/java/com/mountainpier/chart/Application.java
@@ -0,0 +1,14 @@
+package com.mountainpier.chart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
+
+@SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+}
