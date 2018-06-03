2b28591f25af7267937c07b776e353b63c86ba9a
matthewpoletin <contact@matthewpoletin.ru>
2018-06-03 20:25:34 +0300

    Token auth added

--- src/main/java/com/mountainpier/chart/config/WebMvcConfig.java
+++ src/main/java/com/mountainpier/chart/config/WebMvcConfig.java
@@ -20,4 +20,5 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(tokenAuthenticationInterceptor);
 	}
+	
 }
